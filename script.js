81148392ceb989a3a48ecc8bed6c7876884ee4d9
Javier <132447183+Toyos90@users.noreply.github.com>
2023-06-16 11:57:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// para añadir una tarea a la lista
+
 function addTask() {
     let taskInput = document.getElementById("taskInput");
     let taskList = document.getElementById("taskList");
@@ -9,15 +9,11 @@ function addTask() {
     let tasks = getTasks();
     tasks.push(task);
     saveTasks(tasks);
-
-    // Actualizar la lista de tareas en la página
     loadTasks();
-
-    // Limpiar el campo de entrada
     taskInput.value = "";
 }
 
-// Func. para cambiar el estado de una tarea
+
 function changeState(index) {
   let tasks = getTasks();
   let task = tasks[index];
@@ -30,7 +26,7 @@ function changeState(index) {
     loadTasks();
 }
 
-// Función para editar una tarea
+
 function editTask(index) {
   let tasks = getTasks();
   let task = tasks[index];
@@ -42,7 +38,7 @@ function editTask(index) {
   }
 }
 
-// func. eliminar una tarea
+
 function deleteTask(index) {
   let tasks = getTasks();
   tasks.splice(index, 1);
@@ -50,12 +46,12 @@ function deleteTask(index) {
   loadTasks();
 }
 
-// Func. para guardar las tareas en el almacenamiento local
+
 function saveTasks(tasks) {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// func. para obtener las tareas desde el almacenamiento local
+
 function getTasks() {
   let tasks = localStorage.getItem('tasks');
   if (tasks) {
@@ -73,19 +69,19 @@ function loadTasks() {
       let li = document.createElement('li');
       li.innerHTML = task.text + ' - ' + task.state;
 
-      // botón para cambiar el estado de la tarea
+
           let stateButton = document.createElement('button');
           stateButton.innerHTML = 'Cambiar Estado';
           stateButton.setAttribute('onclick', 'changeState('+i+')');
           li.appendChild(stateButton);
 
-      // botón para editar la tarea
+
       let editButton = document.createElement('button');
           editButton.innerHTML = 'Editar';
           editButton.setAttribute('onclick', 'editTask('+i+')');
           li.appendChild(editButton);
 
-      // boton para elminar una tarea
+
       let deleteButton = document.createElement('button');
       deleteButton.innerHTML = 'Eliminar';
       deleteButton.setAttribute('onclick', 'deleteTask('+i+')');
